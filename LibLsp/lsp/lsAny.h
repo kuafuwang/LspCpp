729c934a353176410c04f6974034d5eb2ee11dee
kuafuwang <731784510@qq.com>
2021-07-14 17:20:57 +0800

    1. Add method GetFromMap for Any type to Reflect map to  an object .

--- LibLsp/lsp/lsAny.h
+++ LibLsp/lsp/lsAny.h
@@ -19,20 +19,38 @@ namespace lsp
 		};
 		int jsonType = -1;
 
-		
-		template <typename  T>                       
+
+		template <typename  T>
 		bool  Get(T& value)
 		{
 			const auto visitor = GetReader();
 			Reflect(*visitor, value);
 			return true;
 		}
-		
+
+
+		/*
+		 *Example for GetFromMap
+			std::string data = "{\"visitor\":\"default\",\"verbose\":\"true\"}
+			struct A{
+				std::string  visitor;
+				bool   verbose;
+			}
+			REFLECT_MAP_TO_STRUCT(A,visitor,verbose)
+
+			lsp:Any any;
+			any.SetJsonString(data, static_cast<lsp::Any::Type>(-1));
+			A a_object;
+			any.GetFromMap(a_object);
+		*/
+		template <typename  T>
+		bool GetFromMap(T& value);
+
 		template <typename  T>
 		void  Set(T& value)
 		{
-			 auto visitor = GetWriter();
-			Reflect(*visitor,value);
+			auto visitor = GetWriter();
+			Reflect(*visitor, value);
 			SetData(visitor);
 		}
 
@@ -41,19 +59,19 @@ namespace lsp
 
 		void Set(std::unique_ptr<LspMessage> value);
 
-		void SetJsonString(std::string&& _data,Type _type)
+		void SetJsonString(std::string&& _data, Type _type)
 		{
 			jsonType = _type;
 			data.swap(_data);
 		}
-		void SetJsonString(const std::string& _data ,Type _type)
+		void SetJsonString(const std::string& _data, Type _type)
 		{
 			jsonType = _type;
-			data=(_data);
-			
+			data = (_data);
+
 		}
 		const std::string& Data()const
-		{	
+		{
 			return  data;
 		}
 		void swap(Any& arg) noexcept
@@ -70,9 +88,50 @@ namespace lsp
 		void SetData(std::unique_ptr<Writer>&);
 		std::string  data;
 	};
+
 };
 
 
 extern void Reflect(Reader& visitor, lsp::Any& value);
 extern  void Reflect(Writer& visitor, lsp::Any& value);
 
+template <typename T>
+void ReflectMember(std::map < std::string, lsp::Any>& visitor, const char* name, T& value) {
+
+	auto it = visitor.find(name);
+	if (it != visitor.end())
+	{
+		it->second.Get(value);
+	}
+}
+template <typename T>
+void ReflectMember(std::map < std::string, std::string>& visitor, const char* name, T& value) {
+
+	auto it = visitor.find(name);
+	if (it != visitor.end())
+	{
+		lsp::Any any;
+		any.SetJsonString(it->second, static_cast<lsp::Any::Type>(-1));
+		any.Get(value);
+	}
+}
+
+#define REFLECT_MAP_TO_STRUCT(type, ...)               \
+  template <typename TVisitor>                       \
+  void ReflectMap(TVisitor& visitor, type& value) {     \
+    MACRO_MAP(_MAPPABLE_REFLECT_MEMBER, __VA_ARGS__) \
+  }
+
+
+namespace lsp
+{
+	template <typename T>
+	bool Any::GetFromMap(T& value)
+	{
+		const auto visitor = GetReader();
+		std::map < std::string, lsp::Any> _temp;
+		Reflect(*visitor, _temp);
+		ReflectMap(_temp, value);
+		return true;
+	}
+}
